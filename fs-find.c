f3493cee4a84971193face76da30c488ccd5fa72
Kevin Ewing <kewing@middlebury.edu>
2022-05-09 15:25:04 -0400

    Nearly finding root inode, need to figure out cylinder groups

--- fs-find.c
+++ fs-find.c
@@ -23,6 +23,8 @@ main(int argc, char *argv[])
     int fd;
     int file_size;
 
+    struct ufs2_dinode *root_inode;
+
     fd = open(argv[1], O_RDWR);
     if(fd == -1){
         perror("open");
@@ -32,7 +34,6 @@ main(int argc, char *argv[])
     if (stat(argv[1], &st) != 0){
         perror("stat");
     }
-
     file_size = st.st_size;
 
     printf("File size: %d\n", file_size);
@@ -45,6 +46,19 @@ main(int argc, char *argv[])
     
     superblock = (struct fs *) (disk_map + SBLOCK_UFS2);
 
+    printf("Disk start = %x\n", disk_map);
     printf("Superblock = %x\n", superblock);
-    printf("Mount point = %s\n", superblock->fs_fsmnt);
+    printf("Mount point = %s\n\n", superblock->fs_fsmnt);
+
+    root_inode = (struct ufs2_dinode *) (disk_map + ino_to_cg(superblock, UFS_ROOTINO) + (ino_to_fsba(superblock, UFS_ROOTINO) * MINBSIZE) + (ino_to_fsbo(superblock, UFS_ROOTINO) * 512));
+    printf("ino_to_cg(superblock, UFS_ROOTINO) = %d\n", ino_to_cg(superblock, UFS_ROOTINO));
+    printf("ino_to_fsba(superblock, UFS_ROOTINO) = %d\n", ino_to_fsba(superblock, UFS_ROOTINO));
+    printf("ino_to_fsbo(superblock, UFS_ROOTINO) = %d\n\n", ino_to_fsbo(superblock, UFS_ROOTINO));
+    printf("superblock->fs_cgsize = %d\n\n", superblock->fs_cgsize);
+    
+    
+    printf("Root inode = %x\n", root_inode); 
+    printf("Root inode di_nlink = %d\n", root_inode->di_nlink);
+    printf("Root inode di_atime = %d\n", root_inode->di_atime);
+    printf("Root inode di_size = %d\n", root_inode->di_size);
 }
