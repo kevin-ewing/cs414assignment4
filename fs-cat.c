00e30e16ecd605438591ae7bda441a070ac06518
Kevin Ewing <kewing@middlebury.edu>
2022-05-12 11:02:45 -0400

    fs-cat working, ish

--- fs-cat.c
+++ fs-cat.c
@@ -1,5 +1,5 @@
 /*
- * fs-find.c
+ * fs-cat.c
  */
 
 #include <stdio.h>
@@ -32,18 +32,21 @@ main(int argc, char *argv[])
     struct ufs2_dinode *root_inode;
 
     fd = open(argv[1], O_RDWR);
-    if(fd == -1){
+    if(fd == -1)
+    {
         perror("open");
         return 1;
     }
 
-    if (stat(argv[1], &st) != 0){
+    if (stat(argv[1], &st) != 0)
+    {
         perror("stat");
     }
     file_size = st.st_size;
     
     disk_offset = mmap(0, file_size, PROT_READ, MAP_SHARED, fd, 0);
-    if(disk_offset == MAP_FAILED){
+    if(disk_offset == MAP_FAILED)
+    {
         perror("mmap");
         return 1;
     }
@@ -51,19 +54,17 @@ main(int argc, char *argv[])
     superblock = (struct fs *) (disk_offset + SBLOCK_UFS2);
     root_inode = (struct ufs2_dinode *) (inode_to_offset(superblock, UFS_ROOTINO) + disk_offset);
 
-
-    printf("Argv2 %s\n", argv[2]);
-    fflush(stdout);
-
     int file_ino_num = dps_find(root_inode, disk_offset, superblock, argv[2]);
 
-    printf("Argv2 %s\n", argv[2]);
-    fflush(stdout);
+    if (file_ino_num == -1) {
+        printf("The path or file name may be incorrect...\n");
+        return 0;
+    }
 
     struct ufs2_dinode *file_ino = (struct ufs2_dinode *)(inode_to_offset(superblock, file_ino_num) + disk_offset);
+    char* content = (char *)((file_ino->di_db[0] * superblock->fs_fsize) + disk_offset);
 
-    printf("Files Inode = %d\n", file_ino_num);
-    printf("file_ino->di_mtime %d\n", file_ino->di_ctime);
+    printf("%s\n", content);
 }
 
 long
@@ -80,53 +81,45 @@ int
 dps_find(struct ufs2_dinode * inode, char * disk_offset, struct fs *superblock, char * comp_string){
     struct direct * root_dir, * temp_dir;
     struct ufs2_dinode * temp_inode;
-    char * temp_name;
+    char * target_name;
     int temp_reclen;
 
-    temp_name = strsep(&comp_string, "/");
-    if (temp_name == NULL){
+    // update our "target"
+    target_name = strsep(&comp_string, "/");
+    if (target_name == NULL) {
         return -1;
     }
 
-    printf("temp_name = %s\n", temp_name);
+    // set up directory's root
+    root_dir = (struct direct *)((inode->di_db[0] * superblock->fs_fsize) + disk_offset);
+    temp_reclen = root_dir->d_reclen;
 
-    if (!is_inode_dir(inode)) {
-        char* next = strsep(&comp_string, "/");
-        if ((strcmp(temp_name, temp_dir->d_name) == 0) && (next == NULL)) {
-            return temp_dir->d_ino;
-        } else {
-            return -1;
+    while(temp_reclen < sizeof(struct direct)) {
+        temp_dir = (struct direct *)((inode->di_db[0] * superblock->fs_fsize) + disk_offset + temp_reclen);
+        temp_reclen += temp_dir->d_reclen;
+
+        if ((strcmp(temp_dir->d_name,".") == 0) || (strcmp(temp_dir->d_name,"..") == 0))
+        {
+            continue;
         }
+       
+        // name matches, now check to see if it is a directory:
+        if (strcmp(target_name, temp_dir->d_name) == 0) {
+            temp_inode = (struct ufs2_dinode *) (inode_to_offset(superblock, temp_dir->d_ino) + disk_offset);
+            if (is_inode_dir(temp_inode)) {
+                // this is a directory (if we overshoot, will trigger base case)
+                return dps_find(temp_inode, disk_offset, superblock, comp_string);
+            } else {
+                // we have reached a file
+                target_name = strsep(&comp_string, "/");
+                if (target_name == NULL) {
+                    return temp_dir->d_ino;
+                }
+                return -1;
+            }   
+        } 
     }
-    else{
-        root_dir = (struct direct *)((inode->di_db[0] * superblock->fs_fsize) + disk_offset);
-        temp_reclen = root_dir->d_reclen;
 
-
-        while(temp_reclen < sizeof(struct direct)){
-            temp_dir = (struct direct *)((inode->di_db[0] * superblock->fs_fsize) + disk_offset + temp_reclen);
-            temp_reclen += temp_dir->d_reclen;
-
-            printf("f1\n");
-            fflush(stdout);
-
-            if ((strcmp(temp_dir->d_name,".") == 0) || (strcmp(temp_dir->d_name,"..") == 0)) {
-                continue;
-            }
-
-            printf("f2\n");
-            fflush(stdout);
-
-            if (strcmp(temp_name, temp_dir->d_name) == 0){
-                temp_inode = (struct ufs2_dinode *) (inode_to_offset(superblock, temp_dir->d_ino) + disk_offset);
-                dps_find(temp_inode, disk_offset, superblock, comp_string);
-            }  
-
-            printf("f3\n");
-            fflush(stdout);
-
-            printf("comp_string: %s\n", comp_string);
-            fflush(stdout); 
-        }
-    }
+    // didn't find anything, return -1.
+    return -1;
 }
